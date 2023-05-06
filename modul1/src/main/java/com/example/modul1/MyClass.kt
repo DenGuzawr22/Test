0464765fe85eb2aebe21a1fb4584d83ce15fcbfd
Denys <denys.grushchak@studio.unibo.it>
2023-05-06 20:27:08 +0200

    feat: add mono-repo semantic release

--- modul1/src/main/java/com/example/modul1/MyClass.kt
+++ modul1/src/main/java/com/example/modul1/MyClass.kt
@@ -4,7 +4,7 @@ class MyClass {
     companion object{
         @JvmStatic
         fun main(args: Array<String>) {
-            println("Hello computer from module 1")
+            println("Hello computer from module 1!")
         }
     }
 }
