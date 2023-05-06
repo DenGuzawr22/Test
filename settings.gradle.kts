7548508207ad8a3f8be8463f57e767f5c8dd8400
Denys <denys.grushchak@studio.unibo.it>
2023-05-06 13:19:46 +0200

    build(project): add commit git hooks extension

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,14 @@ rootProject.name = "TestProject"
 
 include(":modul1")
 include(":modul2")
+
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.23"
+}
+
+gitHooks {
+    commitMsg {
+        conventionalCommits()
+    }
+    createHooks(true)
+}
